3c4292484531fc57e5aad3f1f3b07cc59fc6ecaf
rr21pepe <pepe@redradix.com>
2023-05-04 12:23:31 +0200

    Add .prettierrc and reformat files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>Await, suspense and last react-router-dom version</h1>;
+  return <h1>Await, suspense and last react-router-dom version</h1>
 }
 
-export default App;
+export default App
