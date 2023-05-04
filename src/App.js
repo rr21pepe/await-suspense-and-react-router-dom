fcacd95b46ef02d1798537cb9b7228b930bb5fc8
rr21pepe <pepe@redradix.com>
2023-05-04 12:40:09 +0200

    Add first route

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Await, suspense and last react-router-dom version</h1>
-}
-
-export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Inicio</h1>,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
